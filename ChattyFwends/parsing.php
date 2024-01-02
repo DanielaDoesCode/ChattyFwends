0267b0d9b2b9706d1d0324aabb9bc615aa251f71
Daniela <95532847+DanielaDoesCode@users.noreply.github.com>
2024-01-02 21:44:40 +0000

    trying to connect to a local mySQL database with php

--- ChattyFwends/parsing.php
+++ ChattyFwends/parsing.php
@@ -1,8 +1,35 @@
-<?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $say = htmlspecialchars($_POST['say']);
-  $to  = htmlspecialchars($_POST['to']);
 
-  echo  $say, ' ', $to;
-?>
+  <?php
+  // Establish a database connection (Update with your DB credentials)
+  $servername = "localhost";
+  $username = "";
+  $password = "";
+  $dbname = "";
+ //im leaving the credentials out of github cause i still wanna make this project public
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  
+  // Get form data and sanitize inputs
+  $username = mysqli_real_escape_string($conn, $_RQUEST['username']);
+  $password = mysqli_real_escape_string($conn, $_REQUEST['password']);
+  
+  
+  // Insert data into the database
+  $sql = "INSERT INTO login_information (username, password) VALUES ('$username', '$password')";
+  
+  if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  // Close database connection
+  $conn->close();
+
+
+  ?>
